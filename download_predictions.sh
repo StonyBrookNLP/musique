42cb6551c9de9e987bb7574f74ed900afff9ba8a
Harsh Trivedi <harshjtrivedi94@gmail.com>
2022-05-06 00:33:26 -0400

    add v1 data and predictions.

--- download_predictions.sh
+++ download_predictions.sh
@@ -7,10 +7,10 @@ set -x
 # and put them at appropriate locations.
 pip install gdown
 
-ZIP_NAME="musique_v1.0.zip"
+ZIP_NAME="musique_predictions_v1.0.zip"
 
-# URL: https://drive.google.com/file/d/1tGdADlNjWFaHLeZZGShh2IRcpO6Lv24h/view?usp=sharing
-gdown --id 1tGdADlNjWFaHLeZZGShh2IRcpO6Lv24h --output $ZIP_NAME
+# URL: https://drive.google.com/file/d/1XZocqLOTAu4y_1EeAj1JM4Xc1JxGJtx6/view?usp=sharing
+gdown --id 1XZocqLOTAu4y_1EeAj1JM4Xc1JxGJtx6 --output $ZIP_NAME
 unzip $(basename $ZIP_NAME)
 rm $ZIP_NAME
 
